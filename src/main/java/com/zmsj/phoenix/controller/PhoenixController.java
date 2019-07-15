e4cd4bc39d50796c033b5f7ed5a8825764659029
wangkun_ext <kun_wang@asdc.com.cn>
2019-07-15 10:47:19 +0800

    添加service接口

--- src/main/java/com/zmsj/phoenix/controller/PhoenixController.java
+++ src/main/java/com/zmsj/phoenix/controller/PhoenixController.java
@@ -5,8 +5,10 @@ import com.zmsj.phoenix.model.FirstLeftDto;
 import com.zmsj.phoenix.model.FirstRightDto;
 import com.zmsj.phoenix.model.SecondLeftDto;
 import com.zmsj.phoenix.model.SecondRightDto;
+import com.zmsj.phoenix.service.PhoenixService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,34 +19,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PhoenixController {
 
+  @Resource
+  private PhoenixService phoenixService;
+
   @GetMapping("/center")
   @ApiOperation("中间区域接口")
   public CenterDto getSummary() {
-    return null;
+
+    return phoenixService.getSummary();
   }
 
   @GetMapping("/firstLeft")
   @ApiOperation("左边区域第一屏接口")
   public FirstLeftDto getFirstLeft() {
-    return null;
+
+    return phoenixService.getFirstLeft();
   }
 
   @GetMapping("/secondLeft")
   @ApiOperation("左边区域第二屏接口")
   public SecondLeftDto getSecondLeft() {
-    return null;
+
+    return phoenixService.getSecondLeft();
   }
 
   @GetMapping("/firstRight")
   @ApiOperation("右边区域第一屏接口")
   public FirstRightDto getFirstRight() {
 
-    return null;
+    return phoenixService.getFirstRight();
   }
 
   @GetMapping("/secondRight")
   @ApiOperation("右边区域第二屏接口")
   public SecondRightDto getSecondRight() {
-    return null;
+    
+    return phoenixService.getSecondRight();
   }
 }
---
+++ src/main/java/com/zmsj/phoenix/service/PhoenixService.java
@@ -0,0 +1,35 @@
+package com.zmsj.phoenix.service;
+
+import com.zmsj.phoenix.model.CenterDto;
+import com.zmsj.phoenix.model.FirstLeftDto;
+import com.zmsj.phoenix.model.FirstRightDto;
+import com.zmsj.phoenix.model.SecondLeftDto;
+import com.zmsj.phoenix.model.SecondRightDto;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author wang.kun
+ */
+@Service
+public class PhoenixService {
+
+  public CenterDto getSummary() {
+    return null;
+  }
+
+  public FirstLeftDto getFirstLeft() {
+    return null;
+  }
+
+  public SecondLeftDto getSecondLeft() {
+    return null;
+  }
+
+  public FirstRightDto getFirstRight() {
+    return null;
+  }
+
+  public SecondRightDto getSecondRight() {
+    return null;
+  }
+}
